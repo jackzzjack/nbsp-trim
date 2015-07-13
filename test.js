3be636572131c6c0158b008b0ce0cc2a5e7c311f
Yu-Ting, Kao <jackzzjack@gmail.com>
2015-07-13 09:35:58 +0000

    Changelog: Add uni test.

--- test.js
+++ test.js
@@ -2,8 +2,11 @@ var ta = require('./index.js');
 var tape = require('tape');
 
 tape('Starting test ...', function(t) {
-	t.equal(ta(' 123 '), '123 ');
-	t.ok(2, "OK");
-	t.pass("First test ok");
+	t.equal(ta(' 123 '), '123');
+	t.equal(ta('123 '), '123');
+	t.equal(ta(' 123'), '123');
+	t.equal(ta('&nbsp;123'), '123');
+	t.equal(ta('123&nbsp;'), '123');
+	t.equal(ta('1&nbsp;23'), '123');
 	t.end();
 });
